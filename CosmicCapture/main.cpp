169ea383790f14a64b30302a92bb8c42ffe4016e
Erik Biederstadt <biederstadterik@gmail.com>
2021-01-18 16:34:34 -0700

    Adds SDL

--- CosmicCapture/main.cpp
+++ CosmicCapture/main.cpp
@@ -1,8 +1,58 @@
 #include <fmt/format.h>
+#include <SDL/SDL.h>
 
 
-int main()
-{
-	fmt::print("Starting Game, {}", 1.2);
+int main(int argc, char** args) {
+
+	// Pointers to our window and surface
+	SDL_Surface* winSurface = NULL;
+	SDL_Window* window = NULL;
+
+	// Initialize SDL. SDL_Init will return -1 if it fails.
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+		fmt::print("Error initializing SDL : {} ", SDL_GetError());
+		system("pause");
+		// End the program
+		return 1;
+	}
+
+	// Create our window
+	window = SDL_CreateWindow("Cosmic Capture", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720, SDL_WINDOW_SHOWN);
+
+	// Make sure creating the window succeeded
+	if (!window) {
+		fmt::print("Error creating window: {}", SDL_GetError());
+		system("pause");
+		// End the program
+		return 1;
+	}
+
+	// Get the surface from the window
+	winSurface = SDL_GetWindowSurface(window);
+
+	// Make sure getting the surface succeeded
+	if (!winSurface) {
+		fmt::print("Error getting surface: ", SDL_GetError());
+		system("pause");
+		// End the program
+		return 1;
+	}
+
+	// Fill the window with a white rectangle
+	SDL_FillRect(winSurface, NULL, SDL_MapRGB(winSurface->format, 255, 255, 255));
+
+	// Update the window display
+	SDL_UpdateWindowSurface(window);
+
+	// Wait
+	system("pause");
+
+	// Destroy the window. This will also destroy the surface
+	SDL_DestroyWindow(window);
+
+	// Quit SDL
+	SDL_Quit();
+
+	// End the program
 	return 0;
 }
