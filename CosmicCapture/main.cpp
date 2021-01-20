0b90a46df1aa12b33230672e1074d68badd71dee
Erik Biederstadt <biederstadterik@gmail.com>
2021-01-20 15:19:07 -0700

    Implements an element buffer

--- CosmicCapture/main.cpp
+++ CosmicCapture/main.cpp
@@ -65,11 +65,15 @@ int main(int argc, char** args) {
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
 
 	float vertices[] = {
-		-0.5f, -0.5f, 0.0f,
-		 0.5f, -0.5f, 0.0f,
-		 0.0f,  0.5f, 0.0f
+		 0.5f,  0.5f, 0.0f,  // top right
+		 0.5f, -0.5f, 0.0f,  // bottom right
+		-0.5f, -0.5f, 0.0f,  // bottom left
+		-0.5f,  0.5f, 0.0f   // top left 	
 	};
-
+	unsigned int indices[] = {
+		0, 1, 3, // First triangle
+		1, 2, 3  // Second triangle
+	};
 
 	// Vertex shader
 	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
@@ -99,14 +103,19 @@ int main(int argc, char** args) {
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
-	// Vertex buffer, vertex array
-	unsigned int VBO, VAO;
+	// Vertex buffer, vertex array, Element buffer
+	unsigned int VBO, VAO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)nullptr);
 	glEnableVertexAttribArray(0);
@@ -125,7 +134,8 @@ int main(int argc, char** args) {
 
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+		glBindVertexArray(0);
 
 		SDL_GL_SwapWindow(window);
 	}
