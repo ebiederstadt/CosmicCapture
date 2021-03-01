9dcaca8fa0153c39d30aee4408bb23d489480dda
TodayNotTomorrow <serena.schimert@ucalgary.ca>
2021-03-01 09:54:24 -0800

    Shadows + openAL dependecies
    Shadowing code
    - simple shaders to get depth map
    - updates to main shadow to render using generated depth map
    - depth map set-up and implementation in main.cpp and Model.cpp
    
    OpenAL dependencies set-up

--- CosmicCapture/main.cpp
+++ CosmicCapture/main.cpp
@@ -16,6 +16,8 @@
 #include "Render.h"
 
 
+
+
 #define M_PI  3.14159265358979323846
 
 float angle = -0.25f;
@@ -33,6 +35,12 @@ int main(int argc, char** args) {
 	physics.Initialize();
 
 	Input input = Input();
+
+	// Enable depth testing and set up related shader for shhadows
+	glEnable(GL_DEPTH_TEST);
+
+	ShaderProgram simpleDepthShader("shaders/simple.vert", "shaders/simple.frag");
+	simpleDepthShader.compile();
 
 	ShaderProgram shaderProgram("shaders/main.vert", "shaders/main.frag");
 	shaderProgram.compile();
@@ -70,6 +78,45 @@ int main(int argc, char** args) {
   models.push_back(flag);
   models.push_back(dropoffZone);
 
+  // Shadow setup start ---------------------------------------------------------------------
+
+  // Configure depth map FBO
+  unsigned int depthMapFBO;
+  glGenFramebuffers(1, &depthMapFBO);
+  const unsigned int SHADOW_WIDTH = 1024 * 4, SHADOW_HEIGHT = 1024 * 4;
+
+  // create depth texture
+  unsigned int depthMap;
+  glGenTextures(1, &depthMap);
+  glBindTexture(GL_TEXTURE_2D, depthMap);
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT,
+	  SHADOW_WIDTH, SHADOW_HEIGHT, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+  float borderColor[] = { 1.0, 1.0, 1.0, 1.0, 1.0 };
+  glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
+
+  // attach depth texture as FBO's depth buffer
+  glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+  glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthMap, 0);
+  glDrawBuffer(GL_NONE);
+  glReadBuffer(GL_NONE);
+  glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+  // Set sampler aliases
+  shaderProgram.use();
+
+  auto mShaderID = static_cast<unsigned int>(shaderProgram);
+  auto samplerLoc = glGetUniformLocation(mShaderID, "textureSampler");
+  glUniform1i(samplerLoc, 0);
+  samplerLoc = glGetUniformLocation(mShaderID, "shadowMap");
+  glUniform1i(samplerLoc, 1);
+
+  // Shadow setup end ---------------------------------------------------------------------
+
+
 	//main loop flag
 	bool quit = false;
 
@@ -101,15 +148,57 @@ int main(int argc, char** args) {
 		// Update camera
 		sCamera->updateCamera(body->getModelMatrix());
 
-		shaderProgram.use();
+		auto counter = 1;
+
+		// first render to depth map ---------------
+
+		simpleDepthShader.use();
+
+		glViewport(0, 0, SHADOW_WIDTH, SHADOW_HEIGHT);
+		glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+		glClear(GL_DEPTH_BUFFER_BIT);
 
 		// Draw arena
-		arena.drawArena();
+		arena.drawArena(simpleDepthShader, true, depthMapFBO);
 
-		auto counter = 1;
 		for (auto& model : models)
 		{
-			model->draw(modelMatrices[counter]);
+			model->draw(modelMatrices[counter], simpleDepthShader, true, depthMapFBO);
+			++counter;
+		}
+
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+		glViewport(0, 0, width, height);
+		//glClearColor(0.0, 0.0, 0.0, 1.0);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		// Now standard rendering -----------------
+
+		shaderProgram.use();
+
+		float near_plane = 200.f, far_plane = 600.f;
+
+		auto nearLoc = glGetUniformLocation(mShaderID, "near_plane");
+		glUniform1f(nearLoc, near_plane);
+
+		auto farLoc = glGetUniformLocation(mShaderID, "far_plane");
+		glUniform1f(farLoc, far_plane);
+
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, depthMap);
+
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, depthMap);
+
+		glActiveTexture(GL_TEXTURE0);
+
+		// Draw arena
+		arena.drawArena(shaderProgram, false, depthMap);
+
+		counter = 1;
+		for (auto& model : models)
+		{
+			model->draw(modelMatrices[counter], shaderProgram, false, depthMap);
 			++counter;
 		}
 
--- CosmicCapture/shaders/main.vert
+++ CosmicCapture/shaders/main.vert
@@ -6,16 +6,19 @@ layout (location = 2) in vec2 aTexCoords;
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
+uniform mat4 lightSpaceMatrix; // world space to light space 
 
 out vec2 texCoord;
 out vec3 fragPos;
 out vec3 normals;
+out vec4 fragPosLightSpace;
 
 void main()
 {
 	texCoord = aTexCoords;
 	fragPos = vec3(model*vec4(aPos, 1.0));
 	normals = vec3(normalize(model*vec4(aNorm,0.0)));
+	fragPosLightSpace = lightSpaceMatrix * vec4(fragPos, 1.0);
 
 	gl_Position = projection * view * model * vec4(aPos, 1.0);
 }
