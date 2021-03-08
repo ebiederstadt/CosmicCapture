70f8190fd449705a606d274257613d83489423ca
ebels94 <ebel.scott94@gmail.com>
2021-03-08 09:57:26 -0700

    prints turned off, invisible barrier started

--- CosmicCapture/InvisibleBarrier.h
+++ CosmicCapture/InvisibleBarrier.h
@@ -1,21 +1,18 @@
 #pragma once
-
 #include "Entity.h"
 #include "GlobalState.h"
 #include "physics/VehicleFilterShader.h"
 
-
-class Flag : public Entity
+class InvisibleBarrier : public Entity
 {
 public:
-	Flag(const ShaderProgram& shaderProgram, std::shared_ptr<Camera> camera);
+	InvisibleBarrier(const ShaderProgram& shaderProgram, std::shared_ptr<Camera> camera);
 
 	void attachPhysics(Physics& instance) override;
 	void draw(Physics& instance, const ShaderProgram& depthTexture, bool depth, const unsigned& depthMap) override;
 	void simulate(Physics& instance) override;
 	void cleanUpPhysics() override;
-
 private:
-	std::unique_ptr<Model> mFlagBody;
+
 };
 
