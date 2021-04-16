f5248b57e8391b753df37916e6b40d53e2f86d17
jeffreytong <jeffrey.tong@ucalgary.ca>
2021-04-16 11:39:12 -0600

    projectile now spawns ten times faster to avoid bug

--- CosmicCapture/Projectile.h
+++ CosmicCapture/Projectile.h
@@ -23,7 +23,7 @@ class Projectile : public Entity
 		PxVehicleDrive4W* mVehicle;
 
 		float activationTimer = 0.0f;
-		constexpr static float ACTIVATION_TIME = 0.3f * 60.0f; // The projectile is not active for ~ second to prevent self collisions
+		constexpr static float ACTIVATION_TIME = 0.03f * 60.0f; // The projectile is not active for ~ second to prevent self collisions
 	
 		float removalTimer = 0;
 		constexpr static float REMOVAL_TIME = 5.0f * 60.0f; // The projectile should only exist in the game world for ~5 seconds before being deleted
